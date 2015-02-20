412b74d60f75a407aa381d3798bd7272aebb31ee
Sorch <support@sorch.info>
2015-02-20 04:22:56 +0000

    added say command... \o/

--- example.py
+++ example.py
@@ -14,5 +14,24 @@ class Bot(coco.internals.Manager):
 		post = msg.post
 		ret = "%s: %s: %s" % (group.name, user, post)
 		print(ret)
+		#make sure the bot ignores itself.. 
+		if user == group.username:
+			return
+
+		if post[0] == ".":
+			data = body[1:].split(" ", 1)
+			if len(data) > 1:
+				cmd, args = data[0], data[1]
+			else:
+				cmd, args = data[0], ""
+
+			if cmd == "say":
+				if(len(args)) < 1:
+					group.post("I can't say nothing -_-\"")
+				else:
+					group.post(args)
+
+
+
 
 bot = Bot(conf.groups, conf.name, conf.password)._init()
