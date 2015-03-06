f529dea4d5fd6ca066c6a42313104bc60f57221b
Vadim <detriment.ehl@gmail.com>
2015-03-06 13:46:08 -0500

    All features but help function.

--- example.py
+++ example.py
@@ -7,11 +7,14 @@ class Bot(coco.internals.Manager):
         group.setNameColor("ff0")
         group.setFontColor("3bf")
         print("Connected to "+group.name)
+        group.post("I am a dildo bus.")
+
     def _init(self):
         self.run()
 
     def _on_Message(self, group, user, msg):
-        cmds = ["say", "smd", "<3", "ship", "LOL", "help"]
+        cmds = ["say", "smd", "<3", "ship", "LOL"]
+        switch = "."
         post = msg.post
         ret = "%s: %s: %s" % (group.name, user, post)
         print(ret)
@@ -19,32 +22,32 @@ class Bot(coco.internals.Manager):
         if user == group.username:
             return
 
-        if post[0] == ".":
+        if post[0] == switch:
             data = post[1:].split(" ", 1)
             if len(data) > 1:
                 cmd, args = data[0], data[1]
             else:
                 cmd, args = data[0], ""
 
-        if cmd == cmds[0]:
-            if(len(args)) < 1:
-                group.post("I can't say nothing -_-\"")
-            else:
-                group.post(args)
+            if cmd == cmds[0]:
+                if(len(args)) < 1:
+                    group.post("I can't say nothing -_-\"")
+                else:
+                    group.post(args)
 
-        elif cmd == cmds[1]:
-            group.post("Suck My Dick.")
-        elif cmd == cmds[2]:
-            group.post("*h*+50")
-        elif cmd == cmds[3]: 
-            if (len(args)) < 2: group.post(args[1] + "x" + args[2])
-            else:
-                group.post("Shipped...\n")
-        #elif cmd == cmds[-1]:
-        #    top = 0
-        #    end = length(cmds) - 1
-        #    for c in cmds:
-        #         if cmds.index(c) == end:
+            elif cmd == cmds[1]:
+                group.post("Suck My Dick.")
+            elif cmd == cmds[2]:
+                group.post("*h*+50")
+            elif cmd == cmds[3]: 
+                y = args.split(' ')
+                if (len(y)) >= 2: 
+                    group.post(y[0] + " x " + y[1])
+                else:
+                    group.post("Shipped...\n")
+            elif cmd == "h" or cmd == "help": 
+                group.post(post)
+             
              
 
 
